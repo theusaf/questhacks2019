897ec83975fbaf27dabe9714af670ee2e5d11217
theusaf <theusafyt@gmail.com>
2019-11-16 11:12:15 -0800

    update

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const request = require('request');
+const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
 const app = express();
 app.use(express.static("website"));
 app.post("/response",(req,res)=>{
-  
+
 });
 
 app.listen(3000);
