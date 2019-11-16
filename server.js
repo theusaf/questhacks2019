89194400b08513f7d35ea58e52532ca311cc046b
theusaf <theusafyt@gmail.com>
2019-11-16 11:35:50 -0800

    backend

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const request = require('request');
+const ws = require('ws');
+const http = require('http');
 const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
 const app = express();
+const server = http.createServer(app);
+
 app.use(express.static("website"));
 app.post("/response",(req,res)=>{
 
 });
 
-app.listen(3000);
+const socket = new ws.Server({server:server});
+
+server.listen(3000);
--- website/index.js
+++ website/index.js
@@ -1,3 +1,8 @@
+// first form
 function submit(){
+
+}
+// second form
+function submit2(){
 
 }
