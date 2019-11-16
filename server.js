5464ca1b9033a0c6f7f1fbf828b076d7ebfa23d7
theusaf <theusafyt@gmail.com>
2019-11-16 12:02:37 -0800

    server

--- server.js
+++ server.js
@@ -15,8 +15,36 @@ app.post("/response",(req,res)=>{
 
 const socket = new ws.Server({server:server});
 
+const messages = [];
+var clientId = 0;
+const clients = {};
+
 socket.on("connection",sock=>{
-  
+  clients[clientId] = new client;
+  sock.id = clientId;
+  sock.on("message",message=>{
+    let data;
+    try{
+      data = JSON.parse(message);
+    }catch(err){
+      return;
+    }
+    messages.push(data);
+  });
+  sock.on("close",()=>{
+    delete clients[sock.id];
+  });
+  sock.send(JSON.stringify({
+    id: sock.id,
+    messages: messages,
+    type: "start"
+  }));
 });
+
+class client{
+  constructor(){
+    this.name = "";
+  }
+}
 
 server.listen(3000);
