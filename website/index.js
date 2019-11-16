d23c353e9cae72e02d7d3a341c084b24f40afbac
theusaf <theusafyt@gmail.com>
2019-11-16 11:42:12 -0800

    js

--- website/index.js
+++ website/index.js
@@ -1,8 +1,9 @@
 // first form
 function submit(){
-
+  const money = document.getElementById("income").value;
 }
 // second form
 function submit2(){
-
+  const percent = document.getElementById("percent").value;
+  var totalSave = money*percent/100;
 }
